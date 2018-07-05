ab71f1489585ea80f32a89115be5ff7dfb56bb60
vanessa <vanessa@snapzio.com>
2018-07-05 13:51:55 +0800

    add integration test

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,12 @@
-const serverTest = require('./client/index.test.js');
-const clientTest = require('./server/index.test.js');
+const mongoose = require('mongoose');
+const serverTest = require('./server/index.test.js');
+const clientTest = require('./client/index.test.js');
+const integrationTest = require('./integration/index.test.js');
+
+mongoose.Promise = global.Promise;
 
 (async function runTests() {
   await serverTest();
   await clientTest();
+  await integrationTest();
 }());
--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -1,15 +1,36 @@
 const { expect } = require('chai');
-const { Client } = require('../lib');
+const clientApp = require('../app/client');
+require('../app/server');
 
-describe('Client Test', () => {
-  xit('Timeout when no server instance active', async () => {
-    try {
-      await namedClientApp.service('ServerTestName.products').create({
+module.exports = () => {
+  describe('Integration Test', () => {
+    it('Timeout error when there is no active app with specified app name', async () => {
+      try {
+        await clientApp.service('nonExistingApp.products').create({
+          name: 'testproduct',
+          price: '100',
+        });
+      } catch (e) {
+        expect(e.message).to.be.equal('Request timed out.');
+      }
+    }).timeout(10000);
+
+    it('Error when service does not exist on server app', async () => {
+      try {
+        await clientApp.service('ServerAppName.categories').create({
+          name: 'testproduct',
+          price: '100',
+        });
+      } catch (e) {
+        expect(e.name).to.be.equal('NotFound');
+      }
+    });
+
+    xit('Can call create method and get response', async () => {
+      await clientApp.service('ServerAppName.products').create({
         name: 'testproduct',
         price: '100',
       });
-    } catch (e) {
-      expect(e.message).to.be.equal('Request timed out.');
-    }
-  }).timeout(10000);
-});
+    });
+  });
+};
