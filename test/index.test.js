2a51a43432c28cd17774b16a457c98daf779b4f0
vanessa <vanessa@snapzio.com>
2018-07-05 15:42:20 +0800

    add more integration tests

--- test/index.test.js
+++ test/index.test.js
@@ -5,8 +5,18 @@ const integrationTest = require('./integration/index.test.js');
 
 mongoose.Promise = global.Promise;
 
-(async function runTests() {
+describe('Running Tests', async () => {
+  before(async () => {
+    mongoose.connect('mongodb://localhost/feathers-mq-test');
+    await mongoose.connection.dropDatabase();
+  });
+
   await serverTest();
   await clientTest();
   await integrationTest();
-}());
+
+  after(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+  });
+});
--- test/integration/index.test.js
+++ test/integration/index.test.js
@@ -3,6 +3,8 @@ const clientApp = require('../app/client');
 require('../app/server');
 
 module.exports = () => {
+  let productId;
+
   describe('Integration Test', () => {
     it('Timeout error when there is no active app with specified app name', async () => {
       try {
@@ -26,11 +28,59 @@ module.exports = () => {
       }
     });
 
-    xit('Can call create method and get response', async () => {
-      await clientApp.service('ServerAppName.products').create({
+    it('Can call create method on normal service and get response', async () => {
+      const result = await clientApp.service('ServerAppName.products').create({
         name: 'testproduct',
         price: '100',
       });
+      productId = result._id;
+      expect(result.name).to.be.equal('testproduct');
+      expect(result.price).to.be.equal('100');
+      expect(result._id).to.not.be.equal(undefined);
+    });
+
+    it('Can call get method on normal service and get response', async () => {
+      const result = await clientApp.service('ServerAppName.products').get(productId);
+      expect(result.name).to.be.equal('testproduct');
+      expect(result.price).to.be.equal('100');
+      expect(result._id.toString()).to.be.equal(productId);
+    });
+
+    it('Can call find method on normal service and get response', async () => {
+      const result = await clientApp.service('ServerAppName.products').find({});
+      expect(result.total).to.be.equal(1);
+      expect(result.limit).to.be.equal(2);
+      expect(result.skip).to.be.equal(0);
+      expect(result.data.length).to.be.equal(1);
+      expect(result.data[0].name).to.be.equal('testproduct');
+      expect(result.data[0].price).to.be.equal('100');
+      expect(result.data[0]._id.toString()).to.be.equal(productId);
+    });
+
+    it('Can call patch method on normal service and get response', async () => {
+      const result = await clientApp.service('ServerAppName.products').patch(productId, { name: 'newtestproduct' });
+      expect(result.name).to.be.equal('newtestproduct');
+      expect(result.price).to.be.equal('100');
+      expect(result._id.toString()).to.be.equal(productId);
+    });
+
+    it('Can call update method on normal service and get response', async () => {
+      const result = await clientApp.service('ServerAppName.products').update(productId, { price: '1000' });
+      expect(result.name).to.be.equal(undefined);
+      expect(result.price).to.be.equal('1000');
+      expect(result._id.toString()).to.be.equal(productId);
+    });
+
+    it('Can call remove method on normal service and get response', async () => {
+      const result = await clientApp.service('ServerAppName.products').remove(productId);
+      expect(result.name).to.be.equal(undefined);
+      expect(result.price).to.be.equal('1000');
+      expect(result._id.toString()).to.be.equal(productId);
+      const findResult = await clientApp.service('ServerAppName.products').find({});
+      expect(findResult.total).to.be.equal(0);
+      expect(findResult.limit).to.be.equal(2);
+      expect(findResult.skip).to.be.equal(0);
+      expect(findResult.data.length).to.be.equal(0);
     });
   });
 };
